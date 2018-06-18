73d2cce81e1cf6f71b6ba9e2ce6247a02f85aae9
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-06-17 23:18:17 -0500

    none

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
-var topics = ["cat", "great dane", "monkey", "otter", "shark", "dolphin", "british shorthair", "english mastiff", "cardinal", "tabby"] 
+var topics = ["internet cat", "cat", "great dane", "monkey", "silly otter", "shark", "jumping dolphin", "british shorthair", "nyan cat", "pig", "tabby"] 
 console.log(topics);
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=SFh6tH497Mis1yuqSkLJp9TpTgStg7Cz&limit=5";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log("success got data", response);
+renderButtons();
+
+    $("button").on("click", function() {
+      // Grabbing and storing the data-animal property value from the button
+      var animal = $(this).attr("data-name");
+
+      // Constructing a queryURL using the animal name
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=SFh6tH497Mis1yuqSkLJp9TpTgStg7Cz&limit=10";
+
+      // Performing an AJAX request with the queryURL
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        // After data comes back from the request
+        .then(function(response) {
+          
+          console.log(response);
+          // storing the data from the AJAX request in the results variable
+          var results = response.data;
+
+          // Looping through each result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Creating and storing a div tag
+            var animalDiv = $("<div>");
+
+            // Creating and storing an image tag
+            var animalImage = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            animalImage.attr("src", results[i].images.fixed_height_still.url);
+            // animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+            // animalImage.attr("data-animate", results[i].images.fixed_height.url)
+            // animalImage.attr("data-state", "still")
+            // animalImage.addClass("gif");
+            // Appending the paragraph and image tag to the animalDiv
+            animalDiv.append(animalImage);
+
+            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            $("#gifs-appear-here").prepend(animalImage);
+            console.log(animalDIV);
+            
+          }
+        });
     });
-
-    renderButtons();
 
     function renderButtons() {
 
@@ -46,6 +83,21 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=SFh6
 
         // Calling renderButtons which handles the processing of our movie array
         renderButtons();
-        console.log(topics);
+        console.log(animals);
 
       });
+
+      // $(".gif").on("click", function() {
+      //   // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      //   var state = $(this).attr("data-state");
+      //   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      //   // Then, set the image's data-state to animate
+      //   // Else set src to the data-still value
+      //   if (state === "still") {
+      //     $(this).attr("src", $(this).attr("data-animate"));
+      //     $(this).attr("data-state", "animate");
+      //   } else {
+      //     $(this).attr("src", $(this).attr("data-still"));
+      //     $(this).attr("data-state", "still");
+      //   }
+      // });
